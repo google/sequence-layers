c5bd5438a540099d7e5d44b2301c66f53d6c2b35
RJ Skerry-Ryan <rjryan@google.com>
2025-03-04 22:59:48 +0000

    Add sl.RGLRU block, the main recurrent module used in Griffin / Hawk.
    PiperOrigin-RevId: 733477331

--- sequence_layers/jax/recurrent_test.py
+++ sequence_layers/jax/recurrent_test.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ import chex
 import flax
 import jax
 import jax.numpy as jnp
+from recurrentgemma._src.jax import layers as recurrentgemma_layers
 from sequence_layers.jax import recurrent
 from sequence_layers.jax import test_utils
 from sequence_layers.jax import types
@@ -102,7 +103,7 @@ class LSTMTest(test_utils.SequenceLayerTest):
 
     x = test_utils.random_sequence(batch_size, time, channels)
     # Output dtypes are checked in verify_contract.
-    # TODO(rryan): Figure out why jit=True changes the tolerances we can
+    # TODO(b/398200724): Figure out why jit=True changes the tolerances we can
     # achieve with mixed precision.
     self.verify_contract(
         l,
@@ -142,6 +143,133 @@ class LSTMTest(test_utils.SequenceLayerTest):
     y = l.layer(x, training=False).mask_invalid()
     y_keras = types.Sequence(y_keras, y.mask).mask_invalid()
     self.assertSequencesClose(y, y_keras)
+
+
+class RGLRUTest(test_utils.SequenceLayerTest):
+
+  @parameterized.product(
+      param_dtype=(jnp.float32, jnp.bfloat16),
+      random_mask=(False,),
+      scan_type=(
+          'auto',
+          'linear_native',
+          'associative_native',
+          # 'linear_pallas',  # Doesn't work on CPU.
+      ),
+      only_real=(True, False),
+  )
+  def test_rglru(self, param_dtype, random_mask, scan_type, only_real):
+    key = jax.random.PRNGKey(1234)
+    batch_size, time, units, num_heads = 2, 32, 8, 2
+    l = recurrent.RGLRU.Config(
+        units=units,
+        num_heads=num_heads,
+        scan_type=scan_type,
+        only_real=only_real,
+        param_dtype=param_dtype,
+        name='rglru',
+    ).make()
+
+    x = test_utils.random_sequence(batch_size, 1, units)
+    l = self.init_and_bind_layer(key, l, x)
+
+    self.assertTrue(l.supports_step)
+    self.assertEqual(l.block_size, 1)
+    self.assertEqual(l.output_ratio, 1)
+    self.assertEqual(l.input_latency, 0)
+    self.assertEqual(int(l.output_latency), 0)
+    self.assertEqual(l.name, 'rglru')
+
+    variables = flax.core.meta.unbox(l.variables)
+    complex_units = units if only_real else units // 2
+    expected_variables = {
+        'params': {
+            'input_gate': {
+                'kernel': jnp.zeros(
+                    (num_heads, units // num_heads, complex_units // num_heads),
+                    param_dtype,
+                ),
+                'bias': jnp.zeros(
+                    (num_heads, complex_units // num_heads), param_dtype
+                ),
+            },
+            'a_gate': {
+                'kernel': jnp.zeros(
+                    (num_heads, units // num_heads, complex_units // num_heads),
+                    param_dtype,
+                ),
+                'bias': jnp.zeros(
+                    (num_heads, complex_units // num_heads), param_dtype
+                ),
+            },
+            'a_param': jnp.zeros([complex_units], param_dtype),
+        }
+    }
+    if not only_real:
+      expected_variables['params']['a_imag_param'] = jnp.zeros(
+          complex_units, param_dtype
+      )
+
+    chex.assert_trees_all_equal_shapes_and_dtypes(
+        variables,
+        expected_variables,
+    )
+
+    reference_layer = recurrentgemma_layers.RGLRU(
+        width=units,
+        num_heads=num_heads,
+        scan_type=l._scan_type,
+        param_dtype=param_dtype,
+        only_real=only_real,
+    )
+
+    reference_params = {
+        'params': {
+            'a_param': variables['params']['a_param'],
+            'input_gate': {
+                'w': variables['params']['input_gate']['kernel'],
+                'b': variables['params']['input_gate']['bias'],
+            },
+            'a_gate': {
+                'w': variables['params']['a_gate']['kernel'],
+                'b': variables['params']['a_gate']['bias'],
+            },
+        }
+    }
+    if not only_real:
+      reference_params['params']['a_imag_param'] = variables['params'][
+          'a_imag_param'
+      ]
+
+    reference_layer = reference_layer.bind(reference_params)
+    reference_state = reference_layer.init_cache(batch_size, units)
+
+    for time in range(time, time + 1):
+      x = test_utils.random_sequence(
+          batch_size, time, units, random_mask=random_mask
+      )
+
+      y = self.verify_contract(
+          l,
+          x,
+          training=False,
+          **test_utils.get_grad_tols(l, x, param_dtype),
+      )
+
+      # Compare to recurrentgemma reference implementation.
+      # NOTE: The recurrentgemma layer implementation uses x[: 0] by default as
+      #   the state, while the step implementation uses all-zeros. This is a
+      #   layer/step mismatch! To avoid this, use all zeros as the initial state
+      #   when running the reference layer.
+      segment_pos = jnp.tile(
+          jnp.arange(x.shape[1], dtype=jnp.int32)[jnp.newaxis, :],
+          (batch_size, 1),
+      )
+      expected_values, _ = reference_layer(
+          x.values, segment_pos, reference_state
+      )
+      y_expected = types.Sequence(expected_values, x.mask).mask_invalid()
+      self.assertAllClose(y, y_expected)
 
 
 if __name__ == '__main__':
